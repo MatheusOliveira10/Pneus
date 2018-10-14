ea23d019866d7e9a90a53b04e21ea8638c6a6a30
brunotguima@alunos.fho.edu.br <brunotguima@alunos.fho.edu.br>
2018-10-13 21:32:38 -0300

    show_fix

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@
 Route::get('/', 'TyreController@create')->name('home');
 Route::get('/view', 'TyreController@view')->name('view');
 Route::get('/spa', 'TyreController@teste');
+Route::get('/show/{id}', 'TyreController@show');
 
 Auth::routes();
 
